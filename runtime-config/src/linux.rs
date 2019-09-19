949a0a814577fa865c55ebd771aebb86a1082f60
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2019-09-19 17:29:37 +0900

    [WIP] Implement Rust struct for OCI runtime config

--- runtime-config/src/linux.rs
+++ runtime-config/src/linux.rs
@@ -1,15 +1,20 @@
-use std::path::PathBuf;
+use std::{collections::HashMap, path::PathBuf};
 
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
+/// Schema Linux-specific config section.
+///
+/// For more information about the spec, see the [OCI runtime spec].
+///
+/// [OCI runtime spec]: https://github.com/opencontainers/runtime-spec/blob/v1.0.0/config-linux.md
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(
     feature = "serde",
     derive(Serialize, Deserialize),
     serde(rename_all = "camelCase")
 )]
-pub struct Linux {
+pub struct LinuxConfig {
     #[cfg_attr(
         feature = "serde",
         serde(skip_serializing_if = "Vec::is_empty", default)
@@ -33,8 +38,12 @@ pub struct Linux {
         serde(skip_serializing_if = "Vec::is_empty", default)
     )]
     pub devices: Vec<Device>,
-    // pub cgroup_path: Option<PathBuf>,
-    // pub resources: Option<Resources>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub cgroup_path: Option<PathBuf>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub resources: Option<Resources>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -111,4 +120,276 @@ pub enum DeviceType {
     B,
     U,
     P,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(Serialize, Deserialize),
+    // serde(rename_all = "lowercase")
+)]
+pub struct Resources {
+    #[cfg_attr(
+        feature = "serde",
+        serde(skip_serializing_if = "Vec::is_empty", default)
+    )]
+    pub devices: Vec<DeviceResource>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub memory: Option<Memory>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub cpu: Option<Cpu>,
+
+    #[cfg_attr(
+        feature = "serde",
+        serde(rename = "blockIO", skip_serializing_if = "Option::is_none")
+    )]
+    pub block_io: Option<BlockIo>,
+
+    #[cfg_attr(
+        feature = "serde",
+        serde(skip_serializing_if = "Vec::is_empty", default)
+    )]
+    pub hugepage_limits: Vec<HugepageLimit>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub network: Option<Network>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub pids: Option<Pids>,
+
+    // TODO: intel_rdt
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "HashMap::is_empty"))]
+    pub sysctl: HashMap<String, String>,
+
+    // TODO: seccomp
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub rootfs_propagation: Option<ReadonlyPropagationType>,
+
+    #[cfg_attr(
+        feature = "serde",
+        serde(skip_serializing_if = "Vec::is_empty", default)
+    )]
+    pub masked_paths: Vec<PathBuf>,
+
+    #[cfg_attr(
+        feature = "serde",
+        serde(skip_serializing_if = "Vec::is_empty", default)
+    )]
+    pub readonly_paths: Vec<PathBuf>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub mount_label: Option<String>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub struct DeviceResource {
+    pub allow: bool,
+
+    #[cfg_attr(
+        feature = "serde",
+        serde(rename = "type", skip_serializing_if = "Option::is_none")
+    )]
+    pub type_: Option<String>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub major: Option<i64>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub minor: Option<i64>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub access: Option<String>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub struct Memory {
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub limit: Option<i64>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub reservation: Option<i64>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub swap: Option<i64>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub kernel: Option<i64>,
+
+    #[cfg_attr(
+        feature = "serde",
+        serde(rename = "kernelTCP", skip_serializing_if = "Option::is_none")
+    )]
+    pub kernel_tcp: Option<i64>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub swappiness: Option<u64>,
+
+    #[cfg_attr(
+        feature = "serde",
+        serde(rename = "disableOOMKiller", skip_serializing_if = "Option::is_none")
+    )]
+    pub disable_oom_killer: Option<bool>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(Serialize, Deserialize),
+    serde(rename_all = "camelCase")
+)]
+pub struct Cpu {
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub shares: Option<u64>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub quota: Option<i64>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub period: Option<u64>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub realtime_runtime: Option<i64>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub realtime_period: Option<u64>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub cpus: Option<String>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub mems: Option<String>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(Serialize, Deserialize),
+    serde(rename_all = "camelCase")
+)]
+pub struct BlockIo {
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub weight: Option<u16>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub leaf_weight: Option<u16>,
+
+    #[cfg_attr(
+        feature = "serde",
+        serde(skip_serializing_if = "Vec::is_empty", default)
+    )]
+    pub weight_device: Vec<DeviceWeight>,
+
+    #[cfg_attr(
+        feature = "serde",
+        serde(skip_serializing_if = "Vec::is_empty", default)
+    )]
+    pub throttle_read_bps_device: Vec<DeviceThrottle>,
+
+    #[cfg_attr(
+        feature = "serde",
+        serde(skip_serializing_if = "Vec::is_empty", default)
+    )]
+    pub throttle_write_bps_device: Vec<DeviceThrottle>,
+
+    #[cfg_attr(
+        feature = "serde",
+        serde(
+            rename = "throttleReadIOPSDevice",
+            skip_serializing_if = "Vec::is_empty",
+            default
+        )
+    )]
+    pub throttle_read_iops_device: Vec<DeviceThrottle>,
+
+    #[cfg_attr(
+        feature = "serde",
+        serde(
+            rename = "throttleWriteIOPSDevice",
+            skip_serializing_if = "Vec::is_empty",
+            default
+        )
+    )]
+    pub throttle_write_iops_device: Vec<DeviceThrottle>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(Serialize, Deserialize),
+    serde(rename_all = "camelCase")
+)]
+pub struct DeviceWeight {
+    pub major: i64,
+    pub minor: i64,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub weight: Option<u16>,
+
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
+    pub leaf_weight: Option<u16>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub struct DeviceThrottle {
+    pub major: i64,
+    pub minor: i64,
+    pub rate: u64,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(Serialize, Deserialize),
+    serde(rename_all = "camelCase")
+)]
+pub struct HugepageLimit {
+    pub page_size: String, // TODO: use proper struct ?
+    pub limit: u64,        // in bytes
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub struct Network {
+    #[cfg_attr(
+        feature = "serde",
+        serde(rename = "classID", skip_serializing_if = "Option::is_none")
+    )]
+    pub class_id: Option<u32>,
+
+    #[cfg_attr(
+        feature = "serde",
+        serde(skip_serializing_if = "Vec::is_empty", default)
+    )]
+    pub priorities: Vec<NetworkPriority>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub struct NetworkPriority {
+    pub name: String,
+    pub priority: u32,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub struct Pids {
+    pub limit: i64,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(
+    feature = "serde",
+    derive(Serialize, Deserialize),
+    serde(rename_all = "lowercase")
+)]
+pub enum ReadonlyPropagationType {
+    Slave,
+    Private,
+    Shared,
+    Unbindable,
 }
