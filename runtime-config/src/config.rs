562c084dfd6f4125191b983bb4cdd5ce22c5daed
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2019-09-27 22:19:43 +0900

    [WIP] Implement Rust struct for OCI runtime config

--- runtime-config/src/config.rs
+++ runtime-config/src/config.rs
@@ -1,22 +1,33 @@
+//! Runtime configuration schema.
+//!
+//! See the [OCI runtime spec] for more information.
+//!
+//! [OCI runtime spec]: https://github.com/opencontainers/runtime-spec/blob/v1.0.1/config.md
+
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
 use std::{collections::HashMap, path::PathBuf};
 
+/// Runtime configuration schema.
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct Config {
+    /// Version of the OCI runtime spec.
     pub oci_version: String,
 
+    /// Container's root filesystem.
     #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub root: Option<Root>,
 
+    /// Additional mounts beyond the root filesystem.
     #[cfg_attr(
         feature = "serde",
         serde(skip_serializing_if = "Vec::is_empty", default)
     )]
     pub mounts: Vec<Mount>,
 
+    /// Container process.
     #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub process: Option<Process>,
 
@@ -37,31 +48,37 @@ pub struct Config {
     pub annotations: HashMap<String, String>,
 }
 
+/// Container's root filesystem.
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct Root {
+    /// Path to the root filesystem.
     pub path: PathBuf,
 
-    // not on Windows
+    /// Whether the root filesystem MUST be read-only inside the container.
     #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub readonly: Option<bool>,
 }
 
+/// Additional filesystem mounts beyond the root filesystem of a container.
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct Mount {
+    /// Destination of the mount point as an absolute path inside the container.
     pub destination: PathBuf,
 
-    // POSIX
+    /// [POSIX] Type of the filesystem to be mounted.
     #[cfg_attr(
         feature = "serde",
         serde(rename = "type", skip_serializing_if = "Option::is_none")
     )]
     pub type_: Option<String>,
 
+    /// Device name, directory name, or dummy.
     #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub source: Option<PathBuf>,
 
+    /// Mount options of the filesystem to be mounted.
     #[cfg_attr(
         feature = "serde",
         serde(skip_serializing_if = "Vec::is_empty", default)
@@ -69,6 +86,7 @@ pub struct Mount {
     pub options: Vec<String>,
 }
 
+/// Container process.
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(
     feature = "serde",
@@ -76,15 +94,19 @@ pub struct Mount {
     serde(rename_all = "camelCase")
 )]
 pub struct Process {
+    /// Whether a terminal is attached to the process.
     #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub terminal: Option<bool>,
 
+    /// Console size in characters of the terminal.
     #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub console_size: Option<ConsoleSize>,
 
+    /// As which user the process runs.
     pub user: User,
 
-    pub cwd: String,
+    /// Working directory that will be set for the executable.
+    pub cwd: PathBuf,
 
     #[cfg_attr(
         feature = "serde",
@@ -94,41 +116,52 @@ pub struct Process {
 
     pub args: Vec<String>,
 
-    // POSIX
+    /// [POSIX] Resource limits for the process.
     #[cfg_attr(
         feature = "serde",
         serde(skip_serializing_if = "Vec::is_empty", default)
     )]
     pub rlimits: Vec<Rlimit>,
 
-    // Linux
+    /// [Linux] Name of the AppArmor profile for the process.
+    ///
+    /// See the [AppArmor docs] for more information.
+    ///
+    /// [AppArmor docs]: https://wiki.ubuntu.com/AppArmor
     #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub apparmor_profile: Option<String>,
 
-    // Linux
+    // [Linux]
     #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub capabilities: Option<Capabilities>,
 
-    // Linux
+    /// [Linux] Whether to prevent the process from gaining additional privileges.
     #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub no_new_privileges: Option<bool>,
 
-    // Linux
+    /// [Linux] Adjusts the OOM killer score.
     #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub oom_score_adj: Option<i32>,
 
-    // Linux
+    /// [Linux] SELinux label for the process.
+    ///
+    /// See the [SELinux docs] for more information.
+    ///
+    /// [SELinux docs]: http://selinuxproject.org/page/Main_Page
     #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub selinux_label: Option<String>,
 }
 
+/// Console size in characters of a terminal.
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[allow(missing_docs)]
 pub struct ConsoleSize {
     pub height: usize,
     pub width: usize,
 }
 
+/// As which user a container process runs.
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(
     feature = "serde",
@@ -136,30 +169,35 @@ pub struct ConsoleSize {
     serde(rename_all = "camelCase")
 )]
 pub struct User {
-    // POSIX
+    /// [POSIX] User ID in the container namespace.
     pub uid: u32,
-    // POSIX
+    /// [POSIX] Group ID in the container namespace.
     pub gid: u32,
 
-    // POSIX
+    /// [POSIX] Additional group IDs in the container namespace.
     #[cfg_attr(
         feature = "serde",
         serde(skip_serializing_if = "Vec::is_empty", default)
     )]
     pub additional_gids: Vec<u32>,
 
-    // Windows
+    /// [Windows] User name for the process.
     #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub username: Option<String>,
 }
 
+/// Resource limits for a container process.
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct Rlimit {
+    /// Type of platform resource being limited.
     #[cfg_attr(feature = "serde", serde(rename = "type"))]
     pub type_: String,
 
+    /// Value of the limit enforced for the corresponding resource.
     pub soft: u64,
+
+    /// Ceiling for the soft limit.
     pub hard: u64,
 }
 
--- runtime-config/src/lib.rs
+++ runtime-config/src/lib.rs
@@ -1,12 +1,13 @@
 //! OCI runtime config schema in Rust.
 //!
-//! For more information about the spec, see the [OCI runtime spec].
+//! Schema in this crate is based on the v1.0.1 release of the OCI runtime config spec. See the
+//! [OCI runtime spec] for more information about the spec.
 //!
-//! [OCI runtime spec]: https://github.com/opencontainers/runtime-spec/blob/v1.0.0/config.md
+//! [OCI runtime spec]: https://github.com/opencontainers/runtime-spec/releases/tag/v1.0.1
 
 #![warn(
     future_incompatible,
-    // missing_docs,
+    missing_docs,
     missing_debug_implementations,
     nonstandard_style,
     rust_2018_idioms,
@@ -21,8 +22,8 @@ pub mod linux;
 pub use config::Config;
 pub use linux::LinuxConfig;
 
-// /// Schema version for the current specification.
-// pub const SCHEMA_VERSION: u32 = 2;
+/// Version of OCI runtime spec on which this crate is based.
+pub const OCI_VERSION: &str = "0.1.0";
 
 #[cfg(all(feature = "serde", test))]
 mod tests {
@@ -37,4 +38,376 @@ mod tests {
     fn test_config_deser() {
         // TODO
     }
+
+    // Example from https://github.com/opencontainers/runtime-spec/blob/v1.0.1/config.md
+
+    const JSON_DESER: &str = r#"{
+    "ociVersion": "0.5.0-dev",
+    "process": {
+        "terminal": true,
+        "user": {
+            "uid": 1,
+            "gid": 1,
+            "additionalGids": [
+                5,
+                6
+            ]
+        },
+        "args": [
+            "sh"
+        ],
+        "env": [
+            "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+            "TERM=xterm"
+        ],
+        "cwd": "/",
+        "capabilities": {
+            "bounding": [
+                "CAP_AUDIT_WRITE",
+                "CAP_KILL",
+                "CAP_NET_BIND_SERVICE"
+            ],
+            "permitted": [
+                "CAP_AUDIT_WRITE",
+                "CAP_KILL",
+                "CAP_NET_BIND_SERVICE"
+            ],
+            "inheritable": [
+                "CAP_AUDIT_WRITE",
+                "CAP_KILL",
+                "CAP_NET_BIND_SERVICE"
+            ],
+            "effective": [
+                "CAP_AUDIT_WRITE",
+                "CAP_KILL"
+            ],
+            "ambient": [
+                "CAP_NET_BIND_SERVICE"
+            ]
+        },
+        "rlimits": [
+            {
+                "type": "RLIMIT_CORE",
+                "hard": 1024,
+                "soft": 1024
+            },
+            {
+                "type": "RLIMIT_NOFILE",
+                "hard": 1024,
+                "soft": 1024
+            }
+        ],
+        "apparmorProfile": "acme_secure_profile",
+        "oomScoreAdj": 100,
+        "selinuxLabel": "system_u:system_r:svirt_lxc_net_t:s0:c124,c675",
+        "noNewPrivileges": true
+    },
+    "root": {
+        "path": "rootfs",
+        "readonly": true
+    },
+    "hostname": "slartibartfast",
+    "mounts": [
+        {
+            "destination": "/proc",
+            "type": "proc",
+            "source": "proc"
+        },
+        {
+            "destination": "/dev",
+            "type": "tmpfs",
+            "source": "tmpfs",
+            "options": [
+                "nosuid",
+                "strictatime",
+                "mode=755",
+                "size=65536k"
+            ]
+        },
+        {
+            "destination": "/dev/pts",
+            "type": "devpts",
+            "source": "devpts",
+            "options": [
+                "nosuid",
+                "noexec",
+                "newinstance",
+                "ptmxmode=0666",
+                "mode=0620",
+                "gid=5"
+            ]
+        },
+        {
+            "destination": "/dev/shm",
+            "type": "tmpfs",
+            "source": "shm",
+            "options": [
+                "nosuid",
+                "noexec",
+                "nodev",
+                "mode=1777",
+                "size=65536k"
+            ]
+        },
+        {
+            "destination": "/dev/mqueue",
+            "type": "mqueue",
+            "source": "mqueue",
+            "options": [
+                "nosuid",
+                "noexec",
+                "nodev"
+            ]
+        },
+        {
+            "destination": "/sys",
+            "type": "sysfs",
+            "source": "sysfs",
+            "options": [
+                "nosuid",
+                "noexec",
+                "nodev"
+            ]
+        },
+        {
+            "destination": "/sys/fs/cgroup",
+            "type": "cgroup",
+            "source": "cgroup",
+            "options": [
+                "nosuid",
+                "noexec",
+                "nodev",
+                "relatime",
+                "ro"
+            ]
+        }
+    ],
+    "hooks": {
+        "prestart": [
+            {
+                "path": "/usr/bin/fix-mounts",
+                "args": [
+                    "fix-mounts",
+                    "arg1",
+                    "arg2"
+                ],
+                "env": [
+                    "key1=value1"
+                ]
+            },
+            {
+                "path": "/usr/bin/setup-network"
+            }
+        ],
+        "poststart": [
+            {
+                "path": "/usr/bin/notify-start",
+                "timeout": 5
+            }
+        ],
+        "poststop": [
+            {
+                "path": "/usr/sbin/cleanup.sh",
+                "args": [
+                    "cleanup.sh",
+                    "-f"
+                ]
+            }
+        ]
+    },
+    "linux": {
+        "devices": [
+            {
+                "path": "/dev/fuse",
+                "type": "c",
+                "major": 10,
+                "minor": 229,
+                "fileMode": 438,
+                "uid": 0,
+                "gid": 0
+            },
+            {
+                "path": "/dev/sda",
+                "type": "b",
+                "major": 8,
+                "minor": 0,
+                "fileMode": 432,
+                "uid": 0,
+                "gid": 0
+            }
+        ],
+        "uidMappings": [
+            {
+                "hostID": 1000,
+                "containerID": 0,
+                "size": 32000
+            }
+        ],
+        "gidMappings": [
+            {
+                "hostID": 1000,
+                "containerID": 0,
+                "size": 32000
+            }
+        ],
+        "sysctl": {
+            "net.ipv4.ip_forward": "1",
+            "net.core.somaxconn": "256"
+        },
+        "cgroupsPath": "/myRuntime/myContainer",
+        "resources": {
+            "network": {
+                "classID": 1048577,
+                "priorities": [
+                    {
+                        "name": "eth0",
+                        "priority": 500
+                    },
+                    {
+                        "name": "eth1",
+                        "priority": 1000
+                    }
+                ]
+            },
+            "pids": {
+                "limit": 32771
+            },
+            "hugepageLimits": [
+                {
+                    "pageSize": "2MB",
+                    "limit": 9223372036854772000
+                }
+            ],
+            "memory": {
+                "limit": 536870912,
+                "reservation": 536870912,
+                "swap": 536870912,
+                "kernel": -1,
+                "kernelTCP": -1,
+                "swappiness": 0,
+                "disableOOMKiller": false
+            },
+            "cpu": {
+                "shares": 1024,
+                "quota": 1000000,
+                "period": 500000,
+                "realtimeRuntime": 950000,
+                "realtimePeriod": 1000000,
+                "cpus": "2-3",
+                "mems": "0-7"
+            },
+            "devices": [
+                {
+                    "allow": false,
+                    "access": "rwm"
+                },
+                {
+                    "allow": true,
+                    "type": "c",
+                    "major": 10,
+                    "minor": 229,
+                    "access": "rw"
+                },
+                {
+                    "allow": true,
+                    "type": "b",
+                    "major": 8,
+                    "minor": 0,
+                    "access": "r"
+                }
+            ],
+            "blockIO": {
+                "weight": 10,
+                "leafWeight": 10,
+                "weightDevice": [
+                    {
+                        "major": 8,
+                        "minor": 0,
+                        "weight": 500,
+                        "leafWeight": 300
+                    },
+                    {
+                        "major": 8,
+                        "minor": 16,
+                        "weight": 500
+                    }
+                ],
+                "throttleReadBpsDevice": [
+                    {
+                        "major": 8,
+                        "minor": 0,
+                        "rate": 600
+                    }
+                ],
+                "throttleWriteIOPSDevice": [
+                    {
+                        "major": 8,
+                        "minor": 16,
+                        "rate": 300
+                    }
+                ]
+            }
+        },
+        "rootfsPropagation": "slave",
+        "seccomp": {
+            "defaultAction": "SCMP_ACT_ALLOW",
+            "architectures": [
+                "SCMP_ARCH_X86",
+                "SCMP_ARCH_X32"
+            ],
+            "syscalls": [
+                {
+                    "names": [
+                        "getcwd",
+                        "chmod"
+                    ],
+                    "action": "SCMP_ACT_ERRNO"
+                }
+            ]
+        },
+        "namespaces": [
+            {
+                "type": "pid"
+            },
+            {
+                "type": "network"
+            },
+            {
+                "type": "ipc"
+            },
+            {
+                "type": "uts"
+            },
+            {
+                "type": "mount"
+            },
+            {
+                "type": "user"
+            },
+            {
+                "type": "cgroup"
+            }
+        ],
+        "maskedPaths": [
+            "/proc/kcore",
+            "/proc/latency_stats",
+            "/proc/timer_stats",
+            "/proc/sched_debug"
+        ],
+        "readonlyPaths": [
+            "/proc/asound",
+            "/proc/bus",
+            "/proc/fs",
+            "/proc/irq",
+            "/proc/sys",
+            "/proc/sysrq-trigger"
+        ],
+        "mountLabel": "system_u:object_r:svirt_sandbox_file_t:s0:c715,c811"
+    },
+    "annotations": {
+        "com.example.key1": "value1",
+        "com.example.key2": "value2"
+    }
+}
+"#;
 }
